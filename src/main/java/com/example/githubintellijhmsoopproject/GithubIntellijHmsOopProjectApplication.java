1b8d9e96facecee3a548a49748613af274b86d72
MikuILS002 <madiartalgat28@gmail.com>
2026-01-09 19:33:26 +0500

    init commit

---
+++ src/main/java/com/example/githubintellijhmsoopproject/GithubIntellijHmsOopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubintellijhmsoopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntellijHmsOopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubIntellijHmsOopProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubintellijhmsoopproject/GithubIntellijHmsOopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubintellijhmsoopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIntellijHmsOopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
